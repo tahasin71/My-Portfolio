126ff640df8597a2522f7a3187b8a0404b86ecfd
Tahasin Ahamad <162780455+tahasin71@users.noreply.github.com>
2026-06-25 15:10:04 +0200

    Add files via upload

---
+++ technical-artefacts/python-algorithms/bubble_sort.py
@@ -0,0 +1,21 @@
+def bubble_sort(numbers):
+    n = len(numbers)
+
+    for i in range(n):
+        for j in range(0, n - i - 1):
+
+            if numbers[j] > numbers[j + 1]:
+                numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+
+    return numbers
+
+
+numbers = [64, 34, 25, 12, 22, 11, 90]
+
+print("Original List:")
+print(numbers)
+
+sorted_list = bubble_sort(numbers.copy())
+
+print("\nSorted List:")
+print(sorted_list)
